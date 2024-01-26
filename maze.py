c6d847794277359e7066bde0f71f8b9d0acc5e1a
Temporal Dev <dev@temporalvoyage.com>
2024-01-26 18:57:08 -0500

    Task 9 done

--- maze.py
+++ maze.py
@@ -2,7 +2,7 @@ from cell import *
 import time
 
 class Maze:
-    def __init__(self,x,y,num_rows,num_cols,cell_size_x,cell_size_y,win):
+    def __init__(self,x,y,num_rows,num_cols,cell_size_x,cell_size_y,win = None):
         self.x = x
         self.y = y
         self.num_rows = num_rows
@@ -11,11 +11,12 @@ class Maze:
         self.cell_size_y = cell_size_y
         self.win = win
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
-        self._cells = [[Cell(self.win) for j in range(0,self.num_cols)] for i in range(0,self.num_rows)]
-        for i in range(0,self.num_rows):
-            for j in range(0,self.num_cols):
+        self._cells = [[Cell(self.win) for j in range(0,self.num_rows)] for i in range(0,self.num_cols)]
+        for i in range(0,self.num_cols):
+            for j in range(0,self.num_rows):
                 self._draw_cell(i,j)
         self._animate()
     
@@ -25,6 +26,14 @@ class Maze:
         self._cells[i][j].draw(x,y,x+self.cell_size_x,y+self.cell_size_y)
     
     def _animate(self):
+        if self.win is None:
+            return
         self.win.redraw()
         time.sleep(0.05)
+    
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0,0)
+        self._cells[self.num_cols-1][self.num_rows-1].has_bottom_wall = False
+        self._draw_cell(self.num_cols-1,self.num_rows-1)
 
---
+++ tests.py
@@ -0,0 +1,48 @@
+import unittest
+from maze import Maze
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+    
+    def test_maze_width(self):
+        num_cols = 12
+        num_rows = 10
+        widthx = 50
+        widthy = 10
+        m1 = Maze(0, 0, num_rows, num_cols, widthx, widthy)
+        self.assertEqual(
+            len(m1._cells)*m1.cell_size_x,
+            num_cols*widthx,
+        )
+        self.assertEqual(
+            len(m1._cells[0])*m1.cell_size_y,
+            num_rows*widthy,
+        )
+    
+    def test_maze_start_end(self):
+        num_cols = 12
+        num_rows = 10
+        widthx = 50
+        widthy = 10
+        m1 = Maze(0, 0, num_rows, num_cols, widthx, widthy)
+        self.assertEqual(
+            m1._cells[0][0].has_top_wall,
+            False,
+        )
+        self.assertEqual(
+            m1._cells[num_cols-1][num_rows-1].has_bottom_wall,
+            False,
+        )
+
+unittest.main()
