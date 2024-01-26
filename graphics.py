bb8a2a5129aa4e52ce09095e31500e8c019276c0
Temporal Dev <dev@temporalvoyage.com>
2024-01-25 19:55:15 -0500

    Task 4 done

--- graphics.py
+++ graphics.py
@@ -1,13 +1,30 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point:
+    def __init__(self,x=0,y=0):
+        self.x = x
+        self.y = y
+    
+class Line:
+    def __init__(self,start:Point,end:Point):
+        self.start = start
+        self.end = end
+    
+    def draw(self,canvas:Canvas,fill_color):
+        canvas.create_line(self.start.x,self.start.y,self.end.x,self.end.y,fill=fill_color,width=2)
+        canvas.pack(fill=BOTH, expand=1)
+
 class Window:
     def __init__(self,width,height):
         self.__root = Tk()
-        self.__root.title = "Maze Solver"
+        self.__root.title("Maze Solver")
         self.__canvas = Canvas(self.__root,bg="white",width=width,height=height)
         self.__canvas.pack(fill=BOTH, expand=1)
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def draw_line(self,line:Line,fill_color="Black"):
+        line.draw(self.__canvas,fill_color)
 
     def redraw(self):
         self.__root.update_idletasks()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from window import *
+from graphics import *
 
 def main():
     win = Window(800,600)
+    pointA = Point(90,60)
+    pointB = Point(700,500)
+    lineA = Line(pointA,pointB)
+    win.draw_line(lineA)
     win.wait_for_close()
 
 main()
